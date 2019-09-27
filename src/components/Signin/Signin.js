f7af54e02cc3d7f92dbdfddd438e43c4c24fd075
Yash Dhamija <yashdhamija4@gmail.com>
2019-09-26 21:59:25 -0400

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://detectface-api.herokuapp.com/signin', {
+    fetch('https://detect-face1.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
